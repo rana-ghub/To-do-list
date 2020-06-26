10236e8b5852aaabb6873c8de411aed1e5f3772c
rana-ghub <ssshashankrana001@gmail.com>
2020-06-26 20:28:41 +0530

    Todo commit

---
+++ src/head.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './head.css'
+class head extends React.Component{
+    render(){
+        return(
+            <h2>My To-Do List</h2>
+        );
+    }
+}
+export default head;
