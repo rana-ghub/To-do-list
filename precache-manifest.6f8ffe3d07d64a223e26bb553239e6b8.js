f5d0dab0ec5017a3f7aafba4a83151888b3083f7
rana-ghub <ssshashankrana001@gmail.com>
2020-06-26 20:30:23 +0530

    Updates

---
+++ precache-manifest.6f8ffe3d07d64a223e26bb553239e6b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "432ebaf3d671fe452b52add6ad86f8fa",
+    "url": "/To-do-list/index.html"
+  },
+  {
+    "revision": "4cbeae648642eee2dd0b",
+    "url": "/To-do-list/static/css/main.d90b7b30.chunk.css"
+  },
+  {
+    "revision": "8bb3856442a6f5984953",
+    "url": "/To-do-list/static/js/2.470e22d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-list/static/js/2.470e22d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cbeae648642eee2dd0b",
+    "url": "/To-do-list/static/js/main.149a67f7.chunk.js"
+  },
+  {
+    "revision": "5733b68288a02f21eb11",
+    "url": "/To-do-list/static/js/runtime-main.8db080eb.js"
+  }
+]);
